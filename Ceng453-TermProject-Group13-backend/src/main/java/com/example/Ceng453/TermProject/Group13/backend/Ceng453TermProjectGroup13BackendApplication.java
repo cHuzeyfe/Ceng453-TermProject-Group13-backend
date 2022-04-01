897ad835365b4da6fb9fcb081b128dc39ca71f16
frknkr <eker.furkan16@gmail.com>
2022-04-01 20:03:08 +0300

    spring boot initialize

---
+++ Ceng453-TermProject-Group13-backend/src/main/java/com/example/Ceng453/TermProject/Group13/backend/Ceng453TermProjectGroup13BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ceng453.TermProject.Group13.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ceng453TermProjectGroup13BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ceng453TermProjectGroup13BackendApplication.class, args);
+	}
+
+}
